8717d4c123e9480ee4ba03b3a422d2d42545b5c4
Muhammadghayyour999 <136122231+Muhammadghayyour999@users.noreply.github.com>
2023-06-10 11:01:19 +0500

    some basic knowledge about tools for data science
    some basic knowledge about tools for data science and I learning DATA SCIENCE.

---
+++ _DataScienceEcosystem.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+# 
+# **Objectives:**
+# * List Some of the popular languages that Data Scientists.
+# * List Some of the commonly used libraries used by Data Scientists.
+# * List Data science Tools.
+#  
+# 
+# 
+
+# Some of the popular languages that Data Scientists use are:
+
+# 1. Python
+# 2. R
+# 3. SQL
+# 4. Scala
+# 
+
+# Some of the commonly used libraries used by Data Scientists include:
+
+# 1. Pandas
+# 2. NumPy
+# 3. Matplotlib
+# 4. SciPy
+# 
+
+# |  Data Science Tools |
+# |----------------------|
+# |Data Management Tools|
+# |Data Integration and Transformation Tools|
+# |Data Visualization Tools|
+# |Data Visualization Tools|
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[1]:
+
+
+(3*4)+5
+
+
+#  This a simple arithmetic expression to mutiply then add integers
+
+# In[2]:
+
+
+200/60
+
+
+#  This will convert 200 minutes to hours by diving by 60.
+
+# ## Author
+# Muhammad ghayyour khan
+
+# In[ ]:
+
+
+
+
